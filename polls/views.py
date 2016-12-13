2712db648b7555b8dcf90d443de8dd9efa03d6c3
Rock <rock.zhang.cn@gmail.com>
2016-12-13 19:02:37 +0800

    enhance the template, before generic template part in tutorial 4

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
-from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
 
 def index(request):
@@ -30,8 +30,28 @@ def detail(request, question_id):
 	# return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
+	# response = "You're looking at the results of question %s."
+	# return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+	# return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form.
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully dealing
+		# with POST data. This prevents data from being posted twice if a
+		# user hits the Back button.
+		print (reverse('polls:results',  args=(question.id,)))
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
